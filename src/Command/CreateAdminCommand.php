8c6e6f48db4a34ea63c39e61f72fb9f299ca3d02
Eddy <eddy@ecomiz.com>
2026-01-15 19:38:09 +0300

    gestion user

---
+++ src/Command/CreateAdminCommand.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+#[AsCommand(
+    name: 'app:create-admin',
+    description: 'Add a short description for your command',
+)]
+class CreateAdminCommand extends Command
+{
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+        private UserPasswordHasherInterface $passwordHasher
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
+            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $user = new User();
+        $user->setName('Admin');
+        $user->setEmail('admin@renzicom.com');
+
+        $hashedPassword = $this->passwordHasher->hashPassword(
+            $user,
+            'admin123'
+        );
+        $user->setPassword($hashedPassword);
+
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+
+        $output->writeln('✅ Utilisateur admin créé avec succès !');
+        $output->writeln('Email: admin@renzicom.com');
+        $output->writeln('Mot de passe: admin123');
+
+        return Command::SUCCESS;
+    }
+}
