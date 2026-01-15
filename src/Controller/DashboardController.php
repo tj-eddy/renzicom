2a3a162e0c642e82936f7fecc21020a446272225
Eddy <eddy@ecomiz.com>
2026-01-15 22:44:53 +0300

    traduction

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,18 @@ use Symfony\Component\Routing\Attribute\Route;
 final class DashboardController extends AbstractController
 {
     #[Route('/dashboard', name: 'app_dashboard')]
-    public function index(): Response
+    public function index(UserRepository $userRepository): Response
     {
+        $totalUsers = $userRepository->count([]);
+        $adminCount = $userRepository->count(['role' => 'ROLE_ADMIN']);
+        $driverCount = $userRepository->count(['role' => 'ROLE_DRIVER']);
+        $statisticsCount = $userRepository->count(['role' => 'ROLE_STATISTICS']);
+
         return $this->render('dashboard/index.html.twig', [
-            'controller_name' => 'DashboardController',
+            'total_users' => $totalUsers,
+            'admin_count' => $adminCount,
+            'driver_count' => $driverCount,
+            'statistics_count' => $statisticsCount,
         ]);
     }
 }
