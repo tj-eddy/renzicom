7413213f8cea7c703b73a9637f83aa1304dc27d1
Eddy <rakoto.tje@gmail.com>
2026-01-22 20:41:07 +0300

    defaultcrud

---
+++ src/Form/ContactType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Contact;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('email')
+            ->add('phoneNumber')
+            ->add('company')
+            ->add('address')
+            ->add('role')
+            ->add('preferedContactMethod')
+            ->add('note')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Contact::class,
+        ]);
+    }
+}
