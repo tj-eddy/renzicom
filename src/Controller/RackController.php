9dd0a3ccd786a16b6c397e18c6459b6c90310887
Eddy <rakoto.tje@gmail.com>
2026-01-23 01:38:36 +0300

    rack design

--- src/Controller/RackController.php
+++ src/Controller/RackController.php
@@ -10,11 +10,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 #[Route('/rack')]
-final class RackController extends AbstractController
+class RackController extends AbstractController
 {
-    #[Route(name: 'app_rack_index', methods: ['GET'])]
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+        private TranslatorInterface $translator
+    ) {
+    }
+
+    #[Route('/', name: 'app_rack_index', methods: ['GET'])]
     public function index(RackRepository $rackRepository): Response
     {
         return $this->render('rack/index.html.twig', [
@@ -23,17 +30,19 @@ final class RackController extends AbstractController
     }
 
     #[Route('/new', name: 'app_rack_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    public function new(Request $request): Response
     {
         $rack = new Rack();
         $form = $this->createForm(RackType::class, $rack);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($rack);
-            $entityManager->flush();
+            $this->entityManager->persist($rack);
+            $this->entityManager->flush();
 
-            return $this->redirectToRoute('app_rack_index', [], Response::HTTP_SEE_OTHER);
+            $this->addFlash('success', $this->translator->trans('messages.success.created'));
+
+            return $this->redirectToRoute('app_rack_index');
         }
 
         return $this->render('rack/new.html.twig', [
@@ -42,24 +51,18 @@ final class RackController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_rack_show', methods: ['GET'])]
-    public function show(Rack $rack): Response
-    {
-        return $this->render('rack/show.html.twig', [
-            'rack' => $rack,
-        ]);
-    }
-
     #[Route('/{id}/edit', name: 'app_rack_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Rack $rack, EntityManagerInterface $entityManager): Response
+    public function edit(Request $request, Rack $rack): Response
     {
         $form = $this->createForm(RackType::class, $rack);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
+            $this->entityManager->flush();
 
-            return $this->redirectToRoute('app_rack_index', [], Response::HTTP_SEE_OTHER);
+            $this->addFlash('success', $this->translator->trans('messages.success.updated'));
+
+            return $this->redirectToRoute('app_rack_index');
         }
 
         return $this->render('rack/edit.html.twig', [
@@ -69,13 +72,15 @@ final class RackController extends AbstractController
     }
 
     #[Route('/{id}', name: 'app_rack_delete', methods: ['POST'])]
-    public function delete(Request $request, Rack $rack, EntityManagerInterface $entityManager): Response
+    public function delete(Request $request, Rack $rack): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$rack->getId(), $request->getPayload()->getString('_token'))) {
-            $entityManager->remove($rack);
-            $entityManager->flush();
+        if ($this->isCsrfTokenValid('delete' . $rack->getId(), $request->request->get('_token'))) {
+            $this->entityManager->remove($rack);
+            $this->entityManager->flush();
+
+            $this->addFlash('success', $this->translator->trans('messages.success.deleted'));
         }
 
-        return $this->redirectToRoute('app_rack_index', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute('app_rack_index');
     }
 }
